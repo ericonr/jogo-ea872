7a88567c4ddf95f6c20c359d68dcc02a9e710f69
Érico Nogueira <erico.erc@gmail.com>
2021-09-19 12:59:31 -0300

    Adicionar leitura de movimento para Input.

--- src/input.cc
+++ src/input.cc
@@ -1,19 +1,30 @@
+#include <stdexcept>
+
 #include "view.h"
 
 Input::Input(const View &v):
 	m_should_quit(false)
 {
-	keyboard = SDL_GetKeyboardState(nullptr);
+	keyboard = SDL_GetKeyboardState(&numkeys);
 }
 
 void Input::refresh()
 {
-	SDL_PumpEvents();
-	if (keyboard[SDL_SCANCODE_Q]) m_should_quit = true;
-
 	while (SDL_PollEvent(&event)) {
 		if (event.type == SDL_QUIT) {
 			m_should_quit = true;
 		}
 	}
+
+	m_should_quit = keyboard[SDL_SCANCODE_Q];
+	m_movement[direction::up] = keyboard[SDL_SCANCODE_W];
+	m_movement[direction::left] = keyboard[SDL_SCANCODE_A];
+	m_movement[direction::down] = keyboard[SDL_SCANCODE_S];
+	m_movement[direction::right] = keyboard[SDL_SCANCODE_D];
+}
+
+bool Input::movement(int direction)
+{
+	if (direction >= direction::max) throw std::logic_error("out of bounds index");
+	return m_movement[direction];
 }
