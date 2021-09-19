7816d6957348df4d14044357ed26ba8593d1607b
Érico Nogueira <erico.erc@gmail.com>
2021-09-19 19:42:47 -0300

    Adicionar esqueleto do super-modelo de personagens
    Characters vai conter um vetor de Character que usaremos para controlar
    todos personagens do jogo.

--- include/models.h
+++ include/models.h
@@ -1,9 +1,21 @@
 #pragma once
 
+#include <vector>
+
 class Character {
 	public:
 		float x, y;
-		Character(float x, float y): x(x), y(y) {}
 		int size;
+		unsigned long id;
 		Character(float x, float y, int size): x(x), y(y), size(size) {}
 };
+
+/*class Characters {
+	unsigned long ids;
+
+	public:
+		std::vector<Character> cs;
+
+		Characters(): cs(0) {}
+		void add_character(Character &c);
+};*/
