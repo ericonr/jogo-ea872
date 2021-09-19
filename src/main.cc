d77160cc18988ceee33fa3f3b5f9283317d701c7
Érico Nogueira <erico.erc@gmail.com>
2021-09-19 11:31:28 -0300

    Utilizar novas classes para mostrar uma tela estática.

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,19 @@
 #include <iostream>
 
-#include "main.h"
+#include "models.h"
+#include "view.h"
+
+static const float T = 0.01;
 
 int main(int argc, char **argv)
 {
-	std::cout << "hallo" << std::endl;
+	Character c{0, 0};
+	View v{c};
+	Input in{v};
+
+	while(!in.should_quit()) {
+		v.render();
+		in.refresh();
+		v.delay(T);
+	}
 }
