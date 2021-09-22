970996a374413592be67235eb88a0cfb7050be31
zec <eos.josehenrique@gmail.com>
2021-09-22 17:21:10 -0300

    adicao do arquivo characters.cc

--- src/controller.cc
+++ src/controller.cc
@@ -28,13 +28,3 @@ void Controller::update(Input &in, float t)
 		*/
 	}
 }
-
-void Characters::add_character(Character &c){
-	Character_vector.push_back(c);
-	c.id = (Character_vector.size())- 1;
-}
-
-void Characters::delete_character(Character &c)
-{
-	Character_vector.pop_back();
-}
