2c75654a04abf78a2b2ba6c98e9df19bb6fb7929
Érico Nogueira <erico.erc@gmail.com>
2021-09-22 17:36:35 -0300

    Adicionar novo background.

--- src/view.cc
+++ src/view.cc
@@ -46,7 +46,7 @@ View::View(Characters &chars, Scenery_element_vector &sev):
 	}
 
 	texture = IMG_LoadTexture(renderer, "resources/img/capi.png");
-	bg = IMG_LoadTexture(renderer, "resources/img/park.jpeg");
+	bg = IMG_LoadTexture(renderer, "resources/img/bg.png");
 	element_texture = IMG_LoadTexture(renderer, "resources/img/capi.png");
 }
 
